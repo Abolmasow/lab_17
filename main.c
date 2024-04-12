32c264aea776b36c733163b96c2cbbf75ad3c49b
Abolmasow <zevssa1998@gmail.com>
2024-04-13 02:49:28 +0300

    adding test_length_of_string

--- main.c
+++ main.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
+#include <assert.h>
+#include "string/string_.h"
 
+void test_length_of_string_is_not_empty() {
+    char s[] = "Hello";
+
+    assert(lengthOfString(s) == 5);
+}
+
+void test_length_of_string_is_empty() {
+    char s[] = "";
+
+    assert(lengthOfString(s) == 0);
+}
+
+void test_length_of_string_is_exceeds_the_size() {
+    char s[10] = "Hello";
+
+    assert(lengthOfString(s) == 5);
+}
+
+void test_length_of_string() {
+    test_length_of_string_is_not_empty();
+    test_length_of_string_is_empty();
+    test_length_of_string_is_exceeds_the_size();
+}
+
+void test () {
+    test_length_of_string();
+}
+
+int main() {
+    test;
+    return 0;
+}
---
+++ string/string_.h
@@ -0,0 +1,6 @@
+#ifndef LAB_17_STRING__H
+#define LAB_17_STRING__H
+
+int lengthOfString(char *str);
+
+#endif //LAB_17_STRING__H
