5d7637c961ac03fe8eb81fc90d015374961d950b
Abolmasow <zevssa1998@gmail.com>
2024-04-13 03:33:41 +0300

    adding func find, findNonSpace, findSpace, findNonSpaceReverse, findSpaceReverse and test for them

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ void test_length_of_string() {
     test_length_of_string_is_exceeds_the_size();
 }
 
-// 2
+//3
 void test_strlen_is_not_empty() {
     char s[] = "\tHello\t";
 
@@ -45,10 +45,129 @@ void test_strlen_() {
     test_strlen_is_empty();
 }
 
+// 4 (a)
+void test_find_is_enabled() {
+    char s[] = "Hello";
+    char *begin = &s[0];
+    char *end = &s[3];
+    assert(find(begin, end,'e') == &s[1]);
+}
+
+void test_find_is_not_enabled() {
+    char s[] = "Hello";
+    char *begin = &s[0];
+    char *end = &s[3];
+    assert(find(begin, end,'o') == &s[3]);
+}
+
+void test_find() {
+    test_find_is_enabled();
+    test_find_is_not_enabled();
+}
+
+// 4 (b)
+void test_find_non_space_is_space() {
+    char s[] = "\t 3 4 5";
+    assert(findNonSpace(s) == &s[3]);
+}
+
+void test_find_non_space_is_no_space() {
+    char s[] = "12345";
+    assert(findNonSpace(s) == &s[0]);
+}
+
+void test_find_non_space_is_space_and_no_elements() {
+    char s[] = " ";
+    assert(findNonSpace(s) == &s[4]);
+}
+
+void test_find_non_space() {
+    test_find_non_space_is_space();
+    test_find_non_space_is_no_space();
+    test_find_non_space_is_space_and_no_elements();
+}
+
+// 4 (c)
+void test_find_space_is_space_and_no_elements() {
+    char s[] = " ";
+    assert(findSpace(s) == &s[0]);
+}
+
+void test_find_space_is_space() {
+    char s[] = "1234 ";
+    assert(findSpace(s) == &s[4]);
+}
+
+void test_find_space_is_no_space() {
+    char s[] = "12345";
+    assert(findSpace(s) == &s[5]);
+}
+
+void test_find_space() {
+    test_find_space_is_space_and_no_elements();
+    test_find_space_is_space();
+    test_find_space_is_no_space();
+}
+
+// 4 (d)
+void test_find_non_space_reverse_is_no_space() {
+    char s[] = "123";
+    assert(findNonSpaceReverse(&s[2],&s[0]) == &s[2]);
+}
+
+void test_find_non_space_reverse_is_last_element() {
+    char s[] = " 3";
+    assert(findNonSpaceReverse(&s[3],&s[0]) == &s[3]);
+}
+
+void test_find_non_space_reverse_is_no_element() {
+    char s[] = " ";
+    assert(findNonSpaceReverse(&s[3],&s[0]) == &s[0]);
+}
+
+void test_find_non_space_reverse_is_not_last() {
+    char s[] = " 3 ";
+    assert(findNonSpaceReverse(&s[3],&s[0]) == &s[2]);
+}
+
+void test_find_non_space_reverse() {
+    test_find_non_space_reverse_is_no_space();
+    test_find_non_space_reverse_is_last_element();
+    test_find_non_space_reverse_is_no_element();
+    test_find_non_space_reverse_is_not_last();
+}
+
+// 4 (e)
+void test_find_space_reverse_is_no_space() {
+    char s[] = "123";
+    assert(findSpaceReverse(&s[2],&s[0]) == &s[0]);
+}
+
+void test_find_space_reverse_is_last_space() {
+    char s[] = "12 ";
+    assert(findSpaceReverse(&s[3],&s[0]) == &s[3]);
+}
+
+void test_find_space_reverse_is_not_last_space() {
+    char s[] = "12 23";
+    assert(findSpaceReverse(&s[5],&s[0]) == &s[3]);
+}
+
+void test_find_space_reverse() {
+    test_find_space_reverse_is_no_space();
+    test_find_space_reverse_is_last_space();
+    test_find_space_reverse_is_not_last_space();
+}
+
 
 void test () {
     test_length_of_string();
     test_strlen_();
+    test_find();
+    test_find_non_space();
+    test_find_space();
+    test_find_non_space_reverse();
+    test_find_space_reverse();
 }
 
 int main() {
--- string/string_.c
+++ string/string_.c
@@ -18,3 +18,36 @@ size_t strlen_(const char *begin) {
     }
     return end - begin;
 }
+
+char* find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch) {
+        begin++;
+    }
+    return begin;
+}
+
+char* findNonSpace(char *begin) {
+    while (*begin != '\0' && isspace(*begin)) {
+        begin++;
+    }
+    return begin;
+}
+char* findSpace(char *begin) {
+    while (*begin != '\0' && !isspace(*begin)) {
+        begin++;
+    }
+    return begin;
+}
+char* findNonSpaceReverse(char *r_begin, const char *r_end) {
+    while (r_begin != r_end && isspace(*r_begin)) {
+        r_begin--;
+    }
+    return r_begin;
+}
+
+char* findSpaceReverse(char *r_begin, const char *r_end) {
+    while (r_begin != r_end && !isspace(*r_begin)) {
+        r_begin--;
+    }
+    return r_begin;
+}
