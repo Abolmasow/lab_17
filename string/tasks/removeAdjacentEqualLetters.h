42242d0401bf1e3d97b9428664f50d6bf4c57ab8
Abolmasow <zevssa1998@gmail.com>
2024-04-27 13:02:27 +0300

    adding func removeAdjacentEqualLetters and test

---
+++ string/tasks/removeAdjacentEqualLetters.h
@@ -0,0 +1,56 @@
+#ifndef LAB_17_REMOVEADJACENTEQUALLETTERS_H
+#define LAB_17_REMOVEADJACENTEQUALLETTERS_H
+#include <ctype.h>
+#include "../string_.h"
+
+void removeAdjacentEqualLetters(char* s) {
+    char* i_write = s;
+    char* i_read = s;
+    char last_symbol = *s;
+
+    while (*i_read != '\0') {
+        while (*i_read == last_symbol)
+            i_read++;
+
+        *i_write = last_symbol;
+        i_write++;
+        last_symbol = *i_read;
+    }
+
+    *i_write = '\0';
+}
+
+void test_2_empty() {
+    char s[] = "";
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING("", s);
+}
+
+
+void test_2_one_letters() {
+    char s[] = "a";
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING("a", s);
+}
+
+void test_2_one_letters_duplicate() {
+    char s[] = "aa";
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING("a", s);
+}
+
+
+void test_2_two_letters_duplicate() {
+    char s[] = "aaa bbb";
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING("a b", s);
+}
+
+void test_2_remove_adjacent_equal_letters() {
+    test_2_empty();
+    test_2_one_letters();
+    test_2_one_letters_duplicate();
+    test_2_two_letters_duplicate();
+}
+
+#endif //LAB_17_REMOVEADJACENTEQUALLETTERS_H
